a1062855d01e5745777d232ab67ecea1a430154f
Seth Fogarty <sfogarty@trinity.edu>
2019-10-24 15:07:47 -0500

    push

---
+++ lec20_bst.hs
@@ -0,0 +1,23 @@
+import Data.List
+--data BSTInt = Leaf | Node Int BSTInt BSTInt deriving (Show, Eq)
+--data BSTChar = Leaf | Node Char BSTChar BSTChar deriving (Show, Eq)
+
+data BST a = Empty | Node a (BST a) (BST a) deriving Show
+
+values :: BST a -> [a]
+values Empty = []
+values (Node x lft rgt) = [x] ++ (values lft) ++ (values rgt)
+
+instance Ord a => Eq (BST a) where
+  (==) treeA treeB = sort (values treeA) == sort (values treeB)
+
+treeA = Node 7 (Node 3 Empty Empty) (Node 10 Empty Empty)
+treeB = Node 3 Empty (Node 7 Empty (Node 10 Empty Empty))
+{-
+class Eq a where  
+    (==) :: a -> a -> Bool  
+    (/=) :: a -> a -> Bool  
+    x == y = not (x /= y)  
+    x /= y = not (x == y)  
+
+-}
